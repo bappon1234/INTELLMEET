85324d51fcdd34e7c07041159ba45755647942b9
Bappon Namosudra <bapponnamosudra3@gmail.com>
2026-04-23 21:50:34 +0530

    redis fix

--- server/config/redis.js
+++ server/config/redis.js
@@ -1,8 +1,13 @@
 import Redis from "ioredis";
 
-const redis = new Redis({
-    host: "127.0.0.1",
-    port: 6379,
+const redis = new Redis(process.env.REDIS_URL);
+
+redis.on("connect", () => {
+  console.log("Redis Connected");
+});
+
+redis.on("error", (err) => {
+  console.log("Redis Error:", err.message);
 });
 
 export default redis;
